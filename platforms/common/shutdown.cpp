03a50f8a282a927f1ae8099ff2b060b41a9282e0
Kirill <exxxim@gmail.com>
2026-09-14 17:57:06 +0700

    fix(vscode): update CMake settings for Python executable configuration
    (cherry picked from commit 5ca7abfbea09f221faff15e6f9b32167f081b9a3)

--- platforms/common/shutdown.cpp
+++ platforms/common/shutdown.cpp
@@ -219,6 +219,14 @@ static void shutdown_worker(void *arg)
 #if defined(CONFIG_BOARDCTL_RESET)
 int px4_reboot_request(reboot_request_t request, uint32_t delay_us)
 {
+#if defined(CONFIG_ARCH_BOARD_PX4_SITL)
+
+	// SITL can restart the process, but has no bootloader.
+	if (request != REBOOT_REQUEST) {
+		return -ENOTSUP;
+	}
+
+#endif
 	pthread_mutex_lock(&shutdown_mutex);
 
 	if (shutdown_args & SHUTDOWN_ARG_IN_PROGRESS) {
